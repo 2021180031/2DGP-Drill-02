0d8b7a7d143a38c8a0d574864290a48cbcc48437
2021180031 <2021180031@tukorea.ac.kr>
2025-09-15 17:12:19 +0900

    boy move counterclockwise

--- character_grass.py
+++ character_grass.py
@@ -12,7 +12,7 @@ while 1:
     x = 0
     y = 90
     pi = 3.141592
-    rad = -(pi / 2)
+    rad = 3*pi/2
 
     while x<780:
         clear_canvas_now()
@@ -48,12 +48,12 @@ while 1:
         delay(0.01)
 
 
-    while rad<(3*pi/2):
+    while rad> -pi/2:
         clear_canvas_now()
         grass.draw_now(400, 30)
-        character.draw_now(255*math.cos(rad)+400, 255*math.sin(rad)+345)
+        character.draw_now(230*math.cos(rad)+400, 230*math.sin(rad)+345)
         update_canvas()
-        rad+=0.01
+        rad-=0.01
         delay(0.01)
 
 
