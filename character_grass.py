dab7fb04fdfa9c59c71cfe2af2177f96c693b6ff
2021180031 <2021180031@tukorea.ac.kr>
2025-09-15 17:00:57 +0900

    boy cruising rectangular and circular.

--- character_grass.py
+++ character_grass.py
@@ -1,7 +1,64 @@
 from pico2d import *
+import math
 
 open_canvas()
+grass = load_image('grass.png')
+character = load_image('character.png')
 
-# fill here
+
+
+while 1:
+
+    x = 0
+    y = 90
+    pi = 3.141592
+    rad = -(pi / 2)
+
+    while x<780:
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, 90)
+        update_canvas()
+        x += 9
+        delay(0.01)
+
+    while y<550:
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, y)
+        update_canvas()
+        y += 9
+        delay(0.01)
+
+
+    while 20<x:
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, y)
+        update_canvas()
+        x -= 9
+        delay(0.01)
+
+    while 90<y:
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, y)
+        update_canvas()
+        y -= 9
+        delay(0.01)
+
+
+    while rad<(3*pi/2):
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(255*math.cos(rad)+400, 255*math.sin(rad)+345)
+        update_canvas()
+        rad+=0.01
+        delay(0.01)
+
 
 close_canvas()
+
+
+
+# fill here
