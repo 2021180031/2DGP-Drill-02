40bf9a1806cc5a857ff890a10d8e586d0412026e
2021180031 <2021180031@tukorea.ac.kr>
2025-09-16 22:33:58 +0900

    Drill-02 파일 재업로드. 사유: Drill-03 파일로 덧씌워짐

---
+++ character_grass.py
@@ -0,0 +1,64 @@
+from pico2d import *
+import math
+
+open_canvas()
+grass = load_image('grass.png')
+character = load_image('character.png')
+
+
+
+while 1:
+
+    x = 0
+    y = 90
+    pi = 3.141592
+    rad = 3*pi/2
+
+    while x<780:
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, 90)
+        update_canvas()
+        x += 9
+        delay(0.01)
+
+    while y<550:
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, y)
+        update_canvas()
+        y += 9
+        delay(0.01)
+
+
+    while 20<x:
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, y)
+        update_canvas()
+        x -= 9
+        delay(0.01)
+
+    while 90<y:
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, y)
+        update_canvas()
+        y -= 9
+        delay(0.01)
+
+
+    while rad> -pi/2:
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(235*math.cos(rad)+400, 235*math.sin(rad)+325)
+        update_canvas()
+        rad-=0.01
+        delay(0.01)
+
+
+close_canvas()
+
+
+
+# fill here
