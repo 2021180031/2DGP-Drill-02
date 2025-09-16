8e65cb915b9ef1223fe4c1d719e6a6051560cd6b
2021180031 <2021180031@tukorea.ac.kr>
2025-09-16 11:02:54 +0900

    일단 boy를 특정 좌표에 출력하는 것 구현, 이게 되어야 원 운동 좌표 위에 boy 그릴 수 있음

--- character_moves.py
+++ character_moves.py
@@ -3,6 +3,8 @@ import math
 
 open_canvas()
 
+boy = load_image('character.png')
+
 
 def move_rectangle():
     print("Moving rectangle")
@@ -11,13 +13,16 @@ def move_rectangle():
 
 def move_circle():
     print("Moving circle")
+    clear_canvas_now()
+    boy.draw_now(400,300)
+    delay(0.1)
     pass
 
 
 while True:
 
-    move_rectangle()
     move_circle()
+    move_rectangle()
 
     pass
 
